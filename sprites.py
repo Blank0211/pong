4081b46ef5d657078ff6b56d52a58d49c3b59fea
blank0211 <wkgchoh@gmail.com>
2022-09-10 18:31:57 +0530

    Created SpriteGroup class in sprites.py

---
+++ sprites.py
@@ -0,0 +1,18 @@
+class SpriteGroup():
+    """Class for managing sprite objects"""
+    def __init__(self, *sprite_objs):
+        self.sprite_list = []
+        for sprite in sprite_objs:
+            self.sprite_list.append(sprite)
+
+    def add(self, sprite_obj):
+        self.sprite_list.append(sprite_obj)
+
+    def update(self, *args):
+        for sprite in self.sprite_list:
+            sprite.update(*args)
+
+    def draw(self, screen):
+        for sprite in self.sprite_list:
+            screen.blit(sprite.image, sprite.rect)
+
