96f113d8b9cd4bac2ddb71253a04b58a7c15c705
blank0211 <wkgchoh@gmail.com>
2022-09-10 18:40:50 +0530

    Created Ball class

--- sprites.py
+++ sprites.py
@@ -1,3 +1,4 @@
+# Classes and instances for sprites
 class SpriteGroup():
     """Class for managing sprite objects"""
     def __init__(self, *sprite_objs):
@@ -15,4 +16,14 @@ class SpriteGroup():
     def draw(self, screen):
         for sprite in self.sprite_list:
             screen.blit(sprite.image, sprite.rect)
+
+
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, radius, pos_x, pos_y):
+        self.image = pygame.Surface((radius*2, radius*2))
+        pygame.draw.circle(self.image, light_cyan, radius)
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect(center=(pos_x, pos_y))
 
